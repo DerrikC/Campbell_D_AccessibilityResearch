c9da6fd878e73789fe54a22fd1747dfcf7a4840e
Derrik Campbell <derrikcampbell@hotmail.com>
2020-04-03 19:28:47 -0400

    added lyrics that follow the audio using lrc in musicLyrics.js file

---
+++ js/musicLyrics2.js
@@ -0,0 +1,15 @@
+// const ap = new APlayer({
+//     container: document.getElementById('#myplayer'),
+//     showlrc: 1,     
+//     theme: '#6C3C97',                                            
+//     music: {
+//         title: 'Are you Experienced?',
+//         author: 'Jimi Hendrix',
+//         pic: 'images/jimihendrix.jpg',
+//         url: 'music/jimihendrix.mp3',
+//         lrc: '[00:00.00]APlayer00:04.01]is\n[00:08.02]amazing'
+//     }
+// });
+
+
+//practice test
